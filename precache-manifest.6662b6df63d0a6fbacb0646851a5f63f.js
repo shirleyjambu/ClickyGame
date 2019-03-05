441b9bffca5bf9f7c653ade5e622b3550743bc34
Elbe <ashelbe08@gmail.com>
2019-03-04 22:20:30 -0500

    Updates

---
+++ precache-manifest.6662b6df63d0a6fbacb0646851a5f63f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "976821e7c7c83f0311bc",
+    "url": "/clickygame/static/js/runtime~main.976821e7.js"
+  },
+  {
+    "revision": "4db113c508873d6bb477",
+    "url": "/clickygame/static/js/main.4db113c5.chunk.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/clickygame/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "4db113c508873d6bb477",
+    "url": "/clickygame/static/css/main.423e713b.chunk.css"
+  },
+  {
+    "revision": "0d19e7d926dc31da8a40085e4a8f0c07",
+    "url": "/clickygame/index.html"
+  }
+];
